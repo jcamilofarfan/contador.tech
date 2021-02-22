be53cace3e123c8d9b88b01ce2f265b00cccb1cc
Juan Camilo Farfan <farfan1976@hotmail.com>
2021-02-22 16:32:55 -0500

    integracion GitHub en pagina del Blog

--- js/main.js
+++ js/main.js
@@ -1,21 +1,71 @@
 const header = document.querySelector("section.profile-main-container div.resume");
-const section = document.querySelector("section div.proyects-main-container");
-const sectionTwo = document.querySelector("section div.jobs-main-container");
-
+const sectionCourses = document.querySelector("section div.courses-main-container");
+const sectionJobs = document.querySelector("section div.jobs-main-container");
+const sectionProyects = document.querySelector("section div.proyects-main-container");
+// Linkedin Request
 let requestURL = "./assets/data/linkedin.json";
 let request = new XMLHttpRequest();
-// let requestURL = 'https://cache1.phantombooster.com/pzuhC3LNq4M/Q2PwRzWTBaSOHZqrIAM4fA/result.json';
-
-request.open("GET", requestURL);
+// let requestURL = 'https://cache1.phantombooster.com/pzuhC3LNq4M/tSNuGLMzwXoSw1gH5CsRwQ/result.json';
+request.open("GET", requestURL, true);
 request.responseType = "text";
 request.send();
-
 request.onload = function () {
   const linkedinText = request.response;
   const linkedin = JSON.parse(linkedinText);
   populateHeader(linkedin);
-  showlicences(linkedin);
   showJobs(linkedin);
+  showlicences(linkedin);
+};
+
+// request GitHub
+
+let urlGithub = 'https://api.github.com/users/jcamilofarfan/repos';
+let gitRequest = new XMLHttpRequest();
+gitRequest.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 403 || this.readyState == 4 && this.status == 404 ) {
+    urlGithub = "./assets/data/github.json";
+    let gitRequest = new XMLHttpRequest();
+    gitRequest.open("GET", urlGithub, true);
+    gitRequest.responseType = "text";
+    gitRequest.send();
+    gitRequest.onload = function () {
+      const dataText = gitRequest.response;
+      const data = JSON.parse(dataText);
+      showRepositories(data);
+    };
+  }
+};
+
+gitRequest.open(
+"GET",
+urlGithub,
+true
+);
+gitRequest.send();
+gitRequest.onload = function () {
+  const dataText = gitRequest.response;
+  const data = JSON.parse(dataText);
+  showRepositories(data);
+};
+function showRepositories(data){
+  const sectionProyects = document.querySelector("section div.proyects-main-container");
+  for (let i = 0; i < data.length; i++) {
+    const myArticle = document.createElement("article");
+    myArticle.className += "proyects-container";
+    const myH4 = document.createElement("h4");
+    const languajes = document.createElement("h4");
+    const mylink = document.createElement("a");
+    myH4.textContent = data[i].name;
+    mylink.textContent = "Ver certificado";
+    mylink.className += "blogs-button";
+    mylink.href = data[i].html_url;
+    mylink.target = "_blank";
+    // languajes.textContent = "prueba"
+    myArticle.appendChild(myH4);
+    myArticle.appendChild(mylink);
+    myArticle.appendChild(languajes);
+    sectionProyects.appendChild(myArticle);
+  }
 };
 
 function populateHeader(jsonObj) {
@@ -33,7 +83,7 @@ function showlicences(jsonObj) {
   const licences = jsonObj[0]["licences"];
   for (let i = 0; i < licences.length; i++) {
     const myArticle = document.createElement("article");
-    myArticle.className += "proyects-container";
+    myArticle.className += "courses-container";
     const myH2 = document.createElement("h4");
     const mylink = document.createElement("a");
     myH2.textContent = licences[i].name;
@@ -44,7 +94,7 @@ function showlicences(jsonObj) {
     myArticle.appendChild(myH2);
     myArticle.appendChild(mylink);
 
-    section.appendChild(myArticle);
+    sectionCourses.appendChild(myArticle);
   }
 }
 
@@ -64,8 +114,34 @@ function showJobs(jsonObj){
     myArticle.appendChild(fecha);
     myArticle.appendChild(salto);
     myArticle.appendChild(description);
-    sectionTwo.appendChild(myArticle);
+    sectionJobs.appendChild(myArticle);
 
 
   }
 }
+
+function showRepositories(data){
+  const sectionProyects = document.querySelector("section div.proyects-main-container");
+  for (let i = 0; i < data.length; i++) {
+    const myArticle = document.createElement("article");
+    myArticle.className += "proyects-container";
+    const myH4 = document.createElement("h4");
+    const salto = document.createElement("br");
+    const fecha = document.createElement("em");
+    const languajes = document.createElement("p");
+    const mylink = document.createElement("a");
+    myH4.textContent = data[i].name;
+    fecha.textContent =  'Creado el :' + data[i].created_at.substring(0, 10);;
+    mylink.textContent = "Ir al Repositorio";
+    mylink.className += "blogs-button";
+    mylink.href = data[i].html_url;
+    mylink.target = "_blank";
+    languajes.textContent = 'Lenguaje Principal :' + data[i].language;
+    myArticle.appendChild(myH4);
+    myArticle.appendChild(salto);
+    myArticle.appendChild(mylink);
+    myArticle.appendChild(languajes);
+    myArticle.appendChild(fecha);
+    sectionProyects.appendChild(myArticle);
+  }
+};
